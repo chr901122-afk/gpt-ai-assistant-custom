memochou1993/gpt-ai-assistant

Refactor summarize command

app/commands/enquire.js
@@ -1,29 +1,40 @@
-import { COMMAND_ADVISE, COMMAND_CONTINUE } from '../../constants/command.js';
+import {
+  COMMAND_ADVISE, COMMAND_BLAME, COMMAND_CONTINUE, COMMAND_SUMMARIZE,
+} from '../../constants/command.js';
 import { t } from '../../languages/index.js';
 import { PARTICIPANT_AI, PARTICIPANT_HUMAN } from '../../services/openai.js';
-import { generateCompletion } from '../../utils/index.js';
+import { generateCompletion, parseEnquiry } from '../../utils/index.js';
 import MessageAction from '../actions/message.js';
 import Context from '../context.js';
 import { getFormattedHistory, updateHistory } from '../histories.js';
 import { getPrompt, setPrompt } from '../prompts.js';
+import { isCallCommand } from './call.js';
+
+const hasCommand = (context) => (command) => context.isCommand(command) || (isCallCommand(context) && context.hasCommand(command));
 
 /**
  * @param {Context} context
  * @returns {boolean}
  */
-const isAdviseCommand = (context) => context.isCommand(COMMAND_ADVISE);
+const isEnquireCommand = (context) => (
+  hasCommand(context)(COMMAND_ADVISE)
+  || hasCommand(context)(COMMAND_BLAME)
+  || hasCommand(context)(COMMAND_SUMMARIZE)
+);
 
 /**
  * @param {Context} context
  * @returns {Promise<Context>}
  */
-const execAdviseCommand = async (context) => {
+const execEnquireCommand = async (context) => {
   updateHistory(context.contextId, (history) => history.records.pop());
+  const enquiry = parseEnquiry(context.event.trimmedText);
+  console.log(29, enquiry);
   const content = getFormattedHistory(context.contextId);
   const prompt = getPrompt(context.userId);
   prompt
     .write(`\n${PARTICIPANT_HUMAN}: `)
-    .write(`${t('__COMPLETION_ADVISE_REQUEST')}\n${t('__COMPLETION_QUOTATION_MARK_OPENING')}\n${content}\n${t('__COMPLETION_QUOTATION_MARK_CLOSING')}`)
+    .write(`${enquiry}\n${t('__COMPLETION_QUOTATION_MARK_OPENING')}\n${content}\n${t('__COMPLETION_QUOTATION_MARK_CLOSING')}`)
     .write(`\n${PARTICIPANT_AI}: `);
   try {
     const { text, isFinishReasonStop } = await generateCompletion({ prompt: prompt.toString() });
@@ -38,6 +49,6 @@ const execAdviseCommand = async (context) => {
 };
 
 export {
-  isAdviseCommand,
-  execAdviseCommand,
+  isEnquireCommand,
+  execEnquireCommand,
 };

app/handle-events.js
@@ -1,7 +1,6 @@
 import { replyMessage } from '../utils/index.js';
 import {
   execActivateCommand,
-  execAdviseCommand,
   execCallCommand,
   execCommandCommand,
   execConfigureCommand,
@@ -10,20 +9,19 @@ import {
   execDeployCommand,
   execDocCommand,
   execDrawCommand,
-  execSummarizeCommand,
+  execEnquireCommand,
   execTalkCommand,
   execVersionCommand,
   isActivateCommand,
-  isAdviseCommand,
   isCallCommand,
-  isCommand,
+  isCommandCommand,
   isConfigureCommand,
   isContinueCommand,
   isDeactivateCommand,
   isDeployCommand,
   isDocCommand,
   isDrawCommand,
-  isSummarizeCommand,
+  isEnquireCommand,
   isTalkCommand,
   isVersionCommand,
 } from './commands/index.js';
@@ -36,17 +34,16 @@ import Event from './event.js';
  */
 const handleContext = async (context) => (
   (isActivateCommand(context) && execActivateCommand(context))
-    || (isAdviseCommand(context) && execAdviseCommand(context))
-    || (isCommand(context) && execCommandCommand(context))
+    || (isCommandCommand(context) && execCommandCommand(context))
     || (isConfigureCommand(context) && execConfigureCommand(context))
     || (isContinueCommand(context) && execContinueCommand(context))
     || (isDeactivateCommand(context) && execDeactivateCommand(context))
     || (isDeployCommand(context) && execDeployCommand(context))
     || (isDocCommand(context) && execDocCommand(context))
     || (isDrawCommand(context) && execDrawCommand(context))
-    || (isSummarizeCommand(context) && execSummarizeCommand(context))
+    || (isEnquireCommand(context) && execEnquireCommand(context))
     || (isVersionCommand(context) && execVersionCommand(context))
-    || (await isCallCommand(context) && execCallCommand(context))
+    || (isCallCommand(context) && execCallCommand(context))
     || (await isTalkCommand(context) && execTalkCommand(context))
     || context
 );

chr901122-afk/gpt-ai-assistant-custom

app/commands/enquire.js
@@ -1,29 +1,40 @@
-import { COMMAND_ADVISE, COMMAND_CONTINUE } from '../../constants/command.js';
+import {
+  COMMAND_ADVISE, COMMAND_BLAME, COMMAND_CONTINUE, COMMAND_SUMMARIZE,
+} from '../../constants/command.js';
 import { t } from '../../languages/index.js';
 import { PARTICIPANT_AI, PARTICIPANT_HUMAN } from '../../services/openai.js';
-import { generateCompletion } from '../../utils/index.js';
+import { generateCompletion, parseEnquiry } from '../../utils/index.js';
 import MessageAction from '../actions/message.js';
 import Context from '../context.js';
 import { getFormattedHistory, updateHistory } from '../histories.js';
 import { getPrompt, setPrompt } from '../prompts.js';
+import { isTalkCommand } from './talk.js';
+
+const hasCommand = (context) => (command) => context.isCommand(command) || (isTalkCommand(context) && context.hasCommand(command));
 
 /**
  * @param {Context} context
  * @returns {boolean}
  */
-const isAdviseCommand = (context) => context.isCommand(COMMAND_ADVISE);
+const isEnquireCommand = (context) => (
+  hasCommand(context)(COMMAND_ADVISE)
+  || hasCommand(context)(COMMAND_BLAME)
+  || hasCommand(context)(COMMAND_SUMMARIZE)
+);
 
 /**
  * @param {Context} context
  * @returns {Promise<Context>}
  */
-const execAdviseCommand = async (context) => {
+const execEnquireCommand = async (context) => {
   updateHistory(context.contextId, (history) => history.records.pop());
+  const enquiry = parseEnquiry(context.event.trimmedText);
+  console.log(29, enquiry);
   const content = getFormattedHistory(context.contextId);
   const prompt = getPrompt(context.userId);
   prompt
     .write(`\n${PARTICIPANT_HUMAN}: `)
-    .write(`${t('__COMPLETION_ADVISE_REQUEST')}\n${t('__COMPLETION_QUOTATION_MARK_OPENING')}\n${content}\n${t('__COMPLETION_QUOTATION_MARK_CLOSING')}`)
+    .write(`${enquiry}\n${t('__COMPLETION_QUOTATION_MARK_OPENING')}\n${content}\n${t('__COMPLETION_QUOTATION_MARK_CLOSING')}`)
     .write(`\n${PARTICIPANT_AI}: `);
   try {
     const { text, isFinishReasonStop } = await generateCompletion({ prompt: prompt.toString() });
@@ -38,6 +49,6 @@ const execAdviseCommand = async (context) => {
 };
 
 export {
-  isAdviseCommand,
-  execAdviseCommand,
+  isEnquireCommand,
+  execEnquireCommand,
 };

app/handle-events.js
@@ -1,7 +1,6 @@
 import { replyMessage } from '../utils/index.js';
 import {
   execActivateCommand,
-  execAdviseCommand,
   execCommandCommand,
   execConfigureCommand,
   execContinueCommand,
@@ -9,20 +8,19 @@ import {
   execDeployCommand,
   execDocCommand,
   execDrawCommand,
-  execSummarizeCommand,
+  execEnquireCommand,
   execSummonCommand,
   execTalkCommand,
   execVersionCommand,
   isActivateCommand,
-  isAdviseCommand,
-  isCommand,
+  isCommandCommand,
   isConfigureCommand,
   isContinueCommand,
   isDeactivateCommand,
   isDeployCommand,
   isDocCommand,
   isDrawCommand,
-  isSummarizeCommand,
+  isEnquireCommand,
   isSummonCommand,
   isTalkCommand,
   isVersionCommand,
@@ -36,17 +34,16 @@ import Event from './event.js';
  */
 const handleContext = async (context) => (
   (isActivateCommand(context) && execActivateCommand(context))
-    || (isAdviseCommand(context) && execAdviseCommand(context))
-    || (isCommand(context) && execCommandCommand(context))
+    || (isCommandCommand(context) && execCommandCommand(context))
     || (isConfigureCommand(context) && execConfigureCommand(context))
     || (isContinueCommand(context) && execContinueCommand(context))
     || (isDeactivateCommand(context) && execDeactivateCommand(context))
     || (isDeployCommand(context) && execDeployCommand(context))
     || (isDocCommand(context) && execDocCommand(context))
     || (isDrawCommand(context) && execDrawCommand(context))
-    || (isSummarizeCommand(context) && execSummarizeCommand(context))
+    || (isEnquireCommand(context) && execEnquireCommand(context))
     || (isVersionCommand(context) && execVersionCommand(context))
-    || (await isSummonCommand(context) && execSummonCommand(context))
+    || (isSummonCommand(context) && execSummonCommand(context))
     || (await isTalkCommand(context) && execTalkCommand(context))
     || context
 );